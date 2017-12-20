dcc1ffe82bfd0522bbfd7f0c424b36e2aa33e488
jnnapoli <joey@jnnapoli.com>
2017-12-20 05:00:25 -0500

    Small fixes - emulator on tablet was causing crashes not code, works on desktop. cleaned warnings/dependency errors

--- app/src/main/java/com/jnnapoli/accountabilibuddies/MainActivity.java
+++ app/src/main/java/com/jnnapoli/accountabilibuddies/MainActivity.java
@@ -1,4 +1,5 @@
 package com.jnnapoli.accountabilibuddies;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,13 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerView;
@@ -24,26 +28,28 @@ public class MainActivity extends AppCompatActivity {
     private EditText addTaskBox;
     private DatabaseReference databaseReference;
     private List<Task> allTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        allTask = new ArrayList<Task>();
+        allTask = new ArrayList<>();
         databaseReference = FirebaseDatabase.getInstance().getReference();
-        addTaskBox = (EditText)findViewById(R.id.add_task_box);
-        recyclerView = (RecyclerView)findViewById(R.id.task_list);
+        addTaskBox = findViewById(R.id.add_task_box);
+        recyclerView = findViewById(R.id.task_list);
         linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
-        Button addTaskButton = (Button)findViewById(R.id.add_task_button);
+        Button addTaskButton = findViewById(R.id.add_task_button);
+
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String enteredTask = addTaskBox.getText().toString();
-                if(TextUtils.isEmpty(enteredTask)){
+                if (TextUtils.isEmpty(enteredTask)) {
                     Toast.makeText(MainActivity.this, "You must enter a task first", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if(enteredTask.length() < 6){
+                if (enteredTask.length() < 6) {
                     Toast.makeText(MainActivity.this, "Task count must be more than 6", Toast.LENGTH_LONG).show();
                     return;
                 }
@@ -57,35 +63,41 @@ public class MainActivity extends AppCompatActivity {
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 getAllTask(dataSnapshot);
             }
+
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 getAllTask(dataSnapshot);
             }
+
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 taskDeletion(dataSnapshot);
             }
+
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
-    private void getAllTask(DataSnapshot dataSnapshot){
-        for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+    private void getAllTask(DataSnapshot dataSnapshot) {
+        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
             String taskTitle = singleSnapshot.getValue(String.class);
             allTask.add(new Task(taskTitle));
             recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, allTask);
             recyclerView.setAdapter(recyclerViewAdapter);
         }
     }
-    private void taskDeletion(DataSnapshot dataSnapshot){
-        for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+
+    private void taskDeletion(DataSnapshot dataSnapshot) {
+        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
             String taskTitle = singleSnapshot.getValue(String.class);
-            for(int i = 0; i < allTask.size(); i++){
-                if(allTask.get(i).getTask().equals(taskTitle)){
+            for (int i = 0; i < allTask.size(); i++) {
+                if (allTask.get(i).getTask().equals(taskTitle)) {
                     allTask.remove(i);
                 }
             }
